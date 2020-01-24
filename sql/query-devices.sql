41cd7ef23d829647eb702940f66d30c1bae38315
Justin Cooper <47330378+jcooperCUSD@users.noreply.github.com>
2020-01-24 15:06:01 -0800

    updated query-devices.sql to include staff

--- sql/query-devices.sql
+++ sql/query-devices.sql
@@ -1,16 +1,25 @@
-SELECT 
-	STU.ID,
-	CONVERT(VARCHAR(10),[DRA].[DT],101) AS [Issued date],
-	[DRI].[BC] AS Barcode	
-FROM (SELECT [STU].* FROM STU WHERE DEL = 0)
-	STU RIGHT JOIN ((SELECT [DRA].* FROM DRA WHERE DEL = 0)
-	DRA LEFT JOIN (SELECT [DRI].* FROM DRI WHERE DEL = 0)
-	DRI ON [DRI].[RID] = [DRA].[RID] AND [DRI].[RIN] = [DRA].[RIN]) ON [STU].[ID] = [DRA].[ID]
+SELECT  [DRI].[SR] AS [Serial Number],
 
-where RD is NUll
-	AND
-	DRA.RID = 1 
-	AND
-	DRI.DEL <> 1
-	AND
-	convert(date, DT) >= DATEADD (day, -3, convert(date,GETDATE()))
+[DRI].[BC] AS [BarCode],
+
+CONVERT(VARCHAR(10),[DRA].[DT],101) AS [Issued],
+
+CONVERT(VARCHAR(10),[DRA].[RD],101) AS [Returned]
+
+FROM (SELECT [DRA].* FROM DRA WHERE DEL = 0)
+
+       DRA LEFT JOIN (SELECT [DRI].* FROM DRI WHERE DEL = 0) DRI ON [DRI].[RID] = [DRA].[RID] AND [DRI].[RIN] = [DRA].[RIN]
+
+where DRA.RD is NUll
+
+AND
+
+DRA.RID = 1
+
+AND
+
+DRI.DEL <> 1
+
+AND
+--DRA.ST = 'T'
+convert(date, DRA.DT) >= DATEADD (day, -3, convert(date,GETDATE()))
