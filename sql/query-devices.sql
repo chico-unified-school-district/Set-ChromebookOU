20a0df75da18c55e038b7f389ed57a09adb8040f
jcooperCUSD <jcooper@chicousd.org>
2021-06-22 10:05:26 -0700

    reworked grade logic and logging

--- sql/query-devices.sql
+++ sql/query-devices.sql
@@ -1,15 +1,27 @@
 SELECT
 --[DRA].[RID] as RID,
+
+[STU].[GR] as [userGrade],
 [DRA].[ST] as [userType],
 [DRI].[SR] AS [Serial Number],
 [DRI].[BC] AS [BarCode],
 CONVERT(VARCHAR(10),[DRA].[DT],101) AS [Issued],
 CONVERT(VARCHAR(10),[DRA].[RD],101) AS [Returned]
-FROM (SELECT [DRA].* FROM DRA WHERE DEL = 0)
- DRA LEFT JOIN (SELECT [DRI].* FROM DRI WHERE DEL = 0) DRI ON [DRI].[RID] = [DRA].[RID] AND [DRI].[RIN] = [DRA].[RIN]
+FROM (SELECT STU.*
+  FROM STU
+  WHERE DEL = 0 and TG = '' )
+ STU RIGHT JOIN ((SELECT [DRA].*
+  FROM DRA
+  WHERE DEL = 0)
+ DRA LEFT JOIN (SELECT [DRI].*
+  FROM DRI
+  WHERE DEL = 0)
+ DRI ON [DRI].[RID] = [DRA].[RID] AND [DRI].[RIN] = [DRA].[RIN]) ON [STU].[ID] = [DRA].[ID]
+
 WHERE
- DRA.DD IS NULL
+
+DRA.DD IS NULL
  AND DRA.RD IS NUll
- AND DRA.RID IN (1,2,3,4)
+ AND DRA.RID IN (1,3,4,6)
  AND DRI.DEL <> 1
- AND convert(date, DRA.DT) >= DATEADD (day, -3, convert(date,GETDATE()))
+ AND convert(date, DRA.DT) >= DATEADD (day, -1, convert(date,GETDATE()))
