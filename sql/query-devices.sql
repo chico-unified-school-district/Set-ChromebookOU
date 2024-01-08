4d832f8e4bb0886b63c375fbb33a44e7b9cc571a
Justin Cooper <jcooper@chicousd.org>
2024-01-08 15:24:27 -0800

    fixed string output issue at the end of gam ou update

--- sql/query-devices.sql
+++ sql/query-devices.sql
@@ -4,11 +4,10 @@ SELECT
 
 [STU].[GR] as [userGrade],
 [DRA].[ST] as [userType],
-[DRI].[SR] AS [Serial Number],
 [DRI].[SR] AS [sn],
-[DRI].[BC] AS [BarCode],
-CONVERT(VARCHAR(10),[DRA].[DT],101) AS [Issued],
-CONVERT(VARCHAR(10),[DRA].[RD],101) AS [Returned]
+[DRI].[BC] AS [barCode],
+CONVERT(VARCHAR(10),[DRA].[DT],101) AS [issued],
+CONVERT(VARCHAR(10),[DRA].[RD],101) AS [returned]
 FROM (SELECT STU.*
   FROM STU
   WHERE DEL = 0 and TG = '' )
