a06689dc66bac36ef72941db1a0527325cbd80d9
Justin Cooper <jcooper@chicousd.org>
2024-01-12 07:44:57 -0800

    less un-verbose logging

--- lib/Load-Module.ps1
+++ lib/Load-Module.ps1
@@ -1,7 +1,7 @@
 function Load-Module {
  process {
   Write-Verbose ('{0}: {1}' -f $MyInvocation.MyCommand.Name, $_)
-  if (-not(Get-Module -Name $_ -ListAvailable)) {
+  if (!(Get-Module -Name $_ -ListAvailable)) {
    Install-Module -Name $_ -Scope CurrentUser -AllowClobber -Confirm:$false -Force
   }
   Import-Module -Name $_ -Force -ErrorAction Stop -Verbose:$false | Out-Null
